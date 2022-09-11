09bea81ebd4497e4c0f545ba0f3ad65f992c355e
Kristina Tabunchik <tabunchik@doanddot.com>
2022-09-11 17:22:29 +0700

    add table for users

--- src/workshop/tables.py
+++ src/workshop/tables.py
@@ -4,16 +4,27 @@ from sqlalchemy import (
     Integer,
     Numeric,
     String,
+    ForeignKey
 )
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    password_hash = Column(String)
+
+
 class Operation(Base):
     __tablename__ = 'operations'
 
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     date = Column(Date)
     kind = Column(String)
     amount = Column(Numeric(10, 2))
