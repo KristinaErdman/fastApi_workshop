066184be2eccc253a64b89530bf100cb62a1159c
Kristina Tabunchik <tabunchik@doanddot.com>
2022-09-11 19:49:11 +0700

    add jwt auth in endpoints for operations

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -2,7 +2,9 @@ from typing import List, Optional
 
 from fastapi import APIRouter, Depends, Response, status
 
+from ..models.auth import User
 from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
+from ..services.auth import get_current_user
 from ..services.operations import OperationService
 
 router = APIRouter(prefix='/operations', tags=["Operation", ])
@@ -11,34 +13,45 @@ router = APIRouter(prefix='/operations', tags=["Operation", ])
 @router.get('/', response_model=List[Operation])
 def get_operations(
         kind: Optional[OperationKind] = None,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.get_list(kind)
+    return service.get_list(user.id, kind)
 
 
 @router.get('/{operation_id}', response_model=Operation)
-def get_operation(operation_id: int, service: OperationService = Depends()):
-    return service.get(operation_id)
+def get_operation(
+        operation_id: int,
+        user: User = Depends(get_current_user),
+        service: OperationService = Depends()
+):
+    return service.get(user.id, operation_id)
 
 
 @router.post('/', response_model=Operation)
 def create_operation(
         operation_data: OperationCreate,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.create(operation_data)
+    return service.create(user.id, operation_data)
 
 
 @router.put('/{operation_id}', response_model=Operation)
 def update_operation(
         operation_id: int,
         operation_data: OperationUpdate,
+        user: User = Depends(get_current_user),
         service: OperationService = Depends()
 ):
-    return service.update(operation_id, operation_data)
+    return service.update(operation_id, operation_data, user.id)
 
 
 @router.delete('/{operation_id}')
-def delete_operation(operation_id: int, service: OperationService = Depends()):
-    service.delete(operation_id)
+def delete_operation(
+        operation_id: int,
+        user: User = Depends(get_current_user),
+        service: OperationService = Depends()
+):
+    service.delete(operation_id, user.id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -12,34 +12,38 @@ class OperationService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self, kind: OperationKind = None) -> List[Operation]:
-        query = self.session.query(Operation)
+    def get_list(self, user_id: int, kind: OperationKind = None) -> List[Operation]:
+        query = self.session.query(Operation).filter_by(user_id=user_id)
         if kind:
             query = query.filter_by(kind=kind)
         operations = query.all()
         return operations
 
-    def get(self, operation_id: int) -> Operation:
-        operation = self.session.query(Operation).get(operation_id)
+    def get(self, operation_id: int, user_id: int, ) -> Operation:
+        operation = self.session.query(Operation).filter_by(id=operation_id, user_id=user_id)
         if not operation:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
         else:
             return operation
 
-    def create(self, operation_data: OperationCreate) -> Operation:
-        operation = Operation(**operation_data.dict())
+    def create(self, user_id: int, operation_data: OperationCreate) -> Operation:
+        operation = Operation(**operation_data.dict(), user_id=user_id)
         self.session.add(operation)
         self.session.commit()
         return operation
 
-    def update(self, operation_id: int, operation_data: OperationUpdate) -> Operation:
+    def update(self, operation_id: int, operation_data: OperationUpdate, user_id: int, ) -> Operation:
         operation = self.get(operation_id)
+        if operation.user_id != user_id:
+            raise HTTPException(status.HTTP_403_FORBIDDEN)
         for field, value in operation_data:
             setattr(operation, field, value)
         self.session.commit()
         return operation
 
-    def delete(self, operation_id: int):
+    def delete(self, operation_id: int, user_id: int, ):
         operation = self.get(operation_id)
+        if operation.user_id != user_id:
+            raise HTTPException(status.HTTP_403_FORBIDDEN)
         self.session.delete(operation)
         self.session.commit()
