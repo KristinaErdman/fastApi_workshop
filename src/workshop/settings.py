ba5a00644acc24d3d86996d465619048dda05ae6
Kristina Tabunchik <tabunchik@doanddot.com>
2022-10-22 18:58:05 +0700

    fix connect to postgresql

--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -4,8 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
-    database_url: str = "postgresql://postgres:1234@localhost/workshop"
-    # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+    database_url: str = "postgresql://user:password@postgresserver/db"
     jwt_secret: str
     jwt_algorithm: str = 'HS256'
     jwt_expiration: int = 3600  # (sec)
